ba2a111bebb5e46f3c927ea9502c8b01682098ed
Rhys <rbrooker105@gmail.com>
2018-07-08 17:20:42 +0800

    add table and styles for users

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 const closeButton = document.getElementById("helpButton");
 const slider = document.getElementById("slideElement");
 const form = document.getElementById("cycleForm");
+const table = document.getElementById("table");
 
 closeButton.addEventListener("click", function() {
   if (slider.classList.contains("active")) {
@@ -18,12 +19,83 @@ slider.addEventListener("transitionend", function() {
   }
 });
 
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function(e) {
   const record = { days: [] };
   const formData = new FormData(form);
   e.preventDefault();
-  formData.forEach((data, key) => 
-    key === 'days' ?
-      record.days.push([data]) :
-      record[key] = data)
+  formData.forEach(
+    (data, key) =>
+      key === "days" ? record.days.push([data]) : (record[key] = data)
+  );
 });
+
+const users = [
+  {
+    'city': 'City',
+    'days': 'Every day',
+    'email': 'neutron@example.com',
+    'group': 'Always',
+    'name': 'James Issac Neutron',
+    'registration': {
+      'date': '13/08/2018',
+      'time': '11:29AM'
+    }
+  },
+  {
+    'city': 'City',
+    'days': 'Week Days',
+    'email': 'carl@example.com',
+    'group': 'Sometimes',
+    'name': 'Carl Wheezer',
+    'registration': {
+      'date': '13/08/2018',
+      'time': '00:29AM'
+    }
+  },
+  {
+    'city': 'City',
+    'days': 'Weekends',
+    'email': 'cindyvortex@example.com',
+    'group': 'Never',
+    'name': 'Cindy Vortex',
+    'registration': {
+      'date': '13/08/2018',
+      'time': '11:29AM'
+    }
+  },
+  {
+    'city': 'City',
+    'days': 'Mon, Wed, Fri',
+    'email': 'sheen@example.com',
+    'group': 'Sometimes',
+    'name': 'Sheen Estevez',
+    'registration': {
+      'date': '13/08/2018',
+      'time': '11:29AM'
+    }
+  }
+]
+
+function createUserRow(user) {
+  return (
+    `<li class='row'>
+      <ul class='row-container'>
+        <li>${user.name}</li>
+        <li>${user.email}</li>
+        <li>${user.city}</li>
+        <li>${user.group}</li>
+        <li>${user.days}</li>
+        <li>
+          <span class='date'>${user.registration.date}</span>
+          <span class='time'>${user.registration.time}</span>
+        </li>
+        <li>T</li>
+      </ul>
+    </li>`
+  )
+}
+
+table.insertAdjacentHTML('beforeend',createUserRow(users[0]));
+table.insertAdjacentHTML('beforeend',createUserRow(users[1]));
+table.insertAdjacentHTML('beforeend',createUserRow(users[2]));
+table.insertAdjacentHTML('beforeend',createUserRow(users[3]));
--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@
 var closeButton = document.getElementById("helpButton");
 var slider = document.getElementById("slideElement");
 var form = document.getElementById("cycleForm");
+var table = document.getElementById("table");
 
 closeButton.addEventListener("click", function () {
   if (slider.classList.contains("active")) {
@@ -20,11 +21,62 @@ slider.addEventListener("transitionend", function () {
   }
 });
 
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
   var record = { days: [] };
   var formData = new FormData(form);
   e.preventDefault();
   formData.forEach(function (data, key) {
-    return key === 'days' ? record.days.push([data]) : record[key] = data;
+    return key === "days" ? record.days.push([data]) : record[key] = data;
   });
 });
+
+var users = [{
+  'city': 'City',
+  'days': 'Every day',
+  'email': 'neutron@example.com',
+  'group': 'Always',
+  'name': 'James Issac Neutron',
+  'registration': {
+    'date': '13/08/2018',
+    'time': '11:29AM'
+  }
+}, {
+  'city': 'City',
+  'days': 'Week Days',
+  'email': 'carl@example.com',
+  'group': 'Sometimes',
+  'name': 'Carl Wheezer',
+  'registration': {
+    'date': '13/08/2018',
+    'time': '00:29AM'
+  }
+}, {
+  'city': 'City',
+  'days': 'Weekends',
+  'email': 'cindyvortex@example.com',
+  'group': 'Never',
+  'name': 'Cindy Vortex',
+  'registration': {
+    'date': '13/08/2018',
+    'time': '11:29AM'
+  }
+}, {
+  'city': 'City',
+  'days': 'Mon, Wed, Fri',
+  'email': 'sheen@example.com',
+  'group': 'Sometimes',
+  'name': 'Sheen Estevez',
+  'registration': {
+    'date': '13/08/2018',
+    'time': '11:29AM'
+  }
+}];
+
+function createUserRow(user) {
+  return "<li class='row'>\n      <ul class='row-container'>\n        <li>" + user.name + "</li>\n        <li>" + user.email + "</li>\n        <li>" + user.city + "</li>\n        <li>" + user.group + "</li>\n        <li>" + user.days + "</li>\n        <li>\n          <span class='date'>" + user.registration.date + "</span>\n          <span class='time'>" + user.registration.time + "</span>\n        </li>\n        <li>T</li>\n      </ul>\n    </li>";
+}
+
+table.insertAdjacentHTML('beforeend', createUserRow(users[0]));
+table.insertAdjacentHTML('beforeend', createUserRow(users[1]));
+table.insertAdjacentHTML('beforeend', createUserRow(users[2]));
+table.insertAdjacentHTML('beforeend', createUserRow(users[3]));
